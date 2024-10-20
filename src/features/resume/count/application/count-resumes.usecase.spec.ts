6bfb96d8bed5aa7019b2043eb2181e1f883b6f11
oscarprdev <vlcoscar8@gmail.com>
2024-10-20 18:58:59 +0200

    Add list and count resumes unit tests

---
+++ src/features/resume/count/application/count-resumes.usecase.spec.ts
@@ -0,0 +1,54 @@
+import { CountResumesInput } from '../shared/types';
+import { CountResumesUseCase } from './count-resumes.usecase';
+import { CountResumesPort } from './count.resumes.port';
+import { MockInstance, beforeEach, describe, expect, it, vi } from 'vitest';
+import { isError } from '~/lib/utils/either';
+
+class MockCountResumesRepository implements CountResumesPort {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async countResumes(_: CountResumesInput) {
+		return 0;
+	}
+}
+
+describe('Count Resumes UseCase', () => {
+	let usecase: CountResumesUseCase;
+	let spyCountResumes: MockInstance;
+
+	beforeEach(() => {
+		const repository = new MockCountResumesRepository();
+		usecase = new CountResumesUseCase(repository);
+		spyCountResumes = vi.spyOn(repository, 'countResumes');
+	});
+
+	it('Should respond with count', async () => {
+		spyCountResumes.mockImplementationOnce(() => Promise.resolve(0));
+
+		const input: CountResumesInput = {
+			userId: '123',
+		};
+
+		const result = await usecase.execute(input);
+		if (isError(result)) return;
+
+		expect(result.success).toBe(0);
+		expect(spyCountResumes).toHaveBeenCalledWith(input);
+	});
+
+	it('Should respond with error message', async () => {
+		spyCountResumes.mockImplementationOnce(() => Promise.reject());
+
+		const input: CountResumesInput = {
+			userId: '123',
+		};
+
+		const result = await usecase.execute(input);
+
+		expect(isError(result)).toBe(true);
+
+		if (!isError(result)) return;
+
+		expect(result.error).toBe('Error counting resumes');
+		expect(spyCountResumes).toHaveBeenCalledWith(input);
+	});
+});
---
+++ src/features/resume/list/application/list-resumes.usecase.spec.ts
@@ -0,0 +1,55 @@
+import { ListResumesInput } from '../shared/types';
+import { ListResumesPort } from './list-resumes.port';
+import { ListResumesUseCase } from './list-resumes.usecase';
+import { MockInstance, beforeEach, describe, expect, it, vi } from 'vitest';
+import { isError } from '~/lib/utils/either';
+
+class MockListResumesRepository implements ListResumesPort {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async listResumes(_: ListResumesInput) {
+		return [];
+	}
+}
+
+describe('List Resumes UseCase', () => {
+	let usecase: ListResumesUseCase;
+	let spyListResumes: MockInstance;
+
+	beforeEach(() => {
+		const repository = new MockListResumesRepository();
+		usecase = new ListResumesUseCase(repository);
+		spyListResumes = vi.spyOn(repository, 'listResumes');
+	});
+
+	it('Should respond with resumes', async () => {
+		spyListResumes.mockImplementationOnce(() => Promise.resolve([]));
+
+		const input: ListResumesInput = {
+			userId: '123',
+		};
+
+		const result = await usecase.execute(input);
+
+		if (isError(result)) return;
+
+		expect(result.success).toEqual([]);
+		expect(spyListResumes).toHaveBeenCalledWith(input);
+	});
+
+	it('Should respond with error message', async () => {
+		spyListResumes.mockImplementationOnce(() => Promise.reject());
+
+		const input: ListResumesInput = {
+			userId: '123',
+		};
+
+		const result = await usecase.execute(input);
+
+		expect(isError(result)).toBe(true);
+
+		if (!isError(result)) return;
+
+		expect(result.error).toBe('Error listing resumes');
+		expect(spyListResumes).toHaveBeenCalledWith(input);
+	});
+});
