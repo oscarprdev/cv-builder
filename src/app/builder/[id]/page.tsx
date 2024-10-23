5b3af2ad30348b88e4140a5acd5ac2dd9ab972d9
oscarprdev <vlcoscar8@gmail.com>
2024-10-23 08:09:16 +0200

    Update builder page using next15

--- src/app/builder/[id]/page.tsx
+++ src/app/builder/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Builder from '~/features/builder/presentation/page/BuilderPage';
 
-export default async function BuilderPage({ params }: { params: { id: string } }) {
-	return <Builder resumeId={params.id} />;
+export default async function BuilderPage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params;
+	return <Builder resumeId={id} />;
 }
