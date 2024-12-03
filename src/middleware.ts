af942691a5d5840c6a2b4b06469caa08afa734ed
oscarprdev <vlcoscar8@gmail.com>
2024-12-03 07:36:19 +0100

    Refactor middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,5 @@
-import { auth } from './auth';
-
-const PUBLIC_ROUTES = ['/', '/signup', '/signin'];
-
-export default auth(req => {
-	if (!req.auth && !PUBLIC_ROUTES.includes(req.nextUrl.pathname)) {
-		const newUrl = new URL('/signin', req.nextUrl.origin);
-
-		return Response.redirect(newUrl);
-	}
-
-	if (!req.auth && req.nextUrl.pathname === '/') {
-		const newUrl = new URL('/signin', req.nextUrl.origin);
-
-		return Response.redirect(newUrl);
-	}
-
-	if (req.auth && req.nextUrl.pathname === '/') {
-		const newUrl = new URL('/dashboard', req.nextUrl.origin);
-
-		return Response.redirect(newUrl);
-	}
-});
+export { auth } from './auth';
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+	matcher: ['/dashboard', '/builder/:id', 'preview/:id'],
 };
